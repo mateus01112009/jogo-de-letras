2f87bb2f68747c44023f4d1b6fe546a7fcd49495
mateus01112009 <machado.maia@escola.pr.gov.br>
2024-11-12 11:32:13 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,1 +1,47 @@
+let palavra;
 
+function setup() {
+  createCanvas(400, 400);
+  palavra = palavraAleatoria();
+}
+
+function palavraAleatoria(){
+let palavras = ["Beleza","Deus grego", "Beldade" ,"Humilde"];
+   return random (palavras);
+} 
+
+function inicializaCores(){
+   background(" blue");
+fill (" pink");
+ textSize(64);
+  textAlign(CENTER,CENTER) ;
+}
+
+function palavraParcial(minimo,maximo){
+let quantidade = map(mouseX, minimo, maximo, 1 ,palavra.length);
+  let parcial = palavra.substring(0, quantidade);
+  return parcial;
+}
+  
+function draw() {
+ inicializaCores();
+ 
+  let texto = palavraParcial(0, width);
+  text(texto,200,200);
+  
+  /* if(mouseX < 50){
+let palavra = "B"
+ text(palavra,200,200);        
+  } else if(mouseX < 100) {
+ let palavra = "be" ;lhe ajudado, por favor, marcar como solucionado ✓. Bons Estudos!
+
+  text(palavra,200,200);
+ } else if(mouseX < 150){
+    let palavra = "bel";
+   text(palavra,200,200);
+ } else if (mouseX < 200){
+      let palavra = "bele";
+   text(palavra, 200, 200);
+ }*/
+  
+  }
